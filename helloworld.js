9ce34aa6b58119f6d70decface900d7bc7880085
Henry McCreery <henrymccy@gmail.com>
2019-10-02 08:55:00 -0700

    added random color function

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,11 @@
 function helloWorld(){
     alert("hello world!");
     }
+function randomColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
+    // document.getElementById("sidenav").style.background = bgColor;
+    }
